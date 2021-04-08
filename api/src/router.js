378ab0b13dd1fcafc6e5caa40b8829be9ab6fa4d
Thinassi <vthinassi@ciandt.com>
2021-04-08 11:19:04 -0300

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const Router = require('express');
 const multer = require('multer');
 // const Storage = require('multer');
@@ -27,5 +28,19 @@ filename((request, file, callback) => {
     callback(null, file.originalname);
 });
 
+router.post('/upload', upload.single('photo'), (request, response) => {
+    try{
+        request.fileValidationError;
+        response.status(400);
+        json({
+            error: request.fileValidationError
+        });
+    } catch {
+        response.status(201);
+        json({
+            success: true
+        });
+    }
+});
 
 module.exports = router;
