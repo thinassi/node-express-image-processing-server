73a0c58bbb145e44b2a1c0834e53f835d4c2bc15
Thinassi <vthinassi@ciandt.com>
2021-04-08 11:35:40 -0300

    Completed module

--- api/app.js
+++ api/app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
-const { router } = require('./src/router');
+const router = require('./src/router');
 
 const app = express();
 
-const pathToIndex = path.resolve(__dirname,'../client/index.html');
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
 app.use('/', router);
 
 app.use(express.static(path.resolve(__dirname, 'uploads')));
-
-app.use('/*',(request, response) => {
-    response.sendFile(pathToIndex);
+app.use('/*', (request, response) => {
+  response.sendFile(pathToIndex);
 });
 
 module.exports = app;
