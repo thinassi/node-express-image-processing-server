0d505607e6b700fda9e36dcfa515eacec8f5b588
Thinassi <vthinassi@ciandt.com>
2021-04-08 12:03:01 -0300

    Completed module

--- api/src/monochromeWorker.js
+++ api/src/monochromeWorker.js
@@ -2,9 +2,10 @@ const gm = require('gm');
 const { workerData } = require('worker_threads');
 const { parentPort } = require('worker_threads');
 
-gm(workerData.source).monochrome.write(workerData.destination,
-    (error) => {
-        if (error) throw (error);
+gm(workerData.source)
+    .monochrome()
+    .write(workerData.destination, (error) => {
+        if (error) throw error;
         parentPort.postMessage({
             monochrome: true
         });
